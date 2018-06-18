35351be745ab93bfffc4202f54db4f77a3ad536d
dfcantillo <dfcantillod@gmail.com>
2018-06-17 19:24:25 -0500

    Primer commit de la plicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
 
+// Rutas
+import { APP_ROUTES } from './app.router';
 
+// Componentes
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
